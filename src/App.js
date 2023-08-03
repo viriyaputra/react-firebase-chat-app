6d7e4df9ade440b03c81ca8e277994821032b382
viriyaputra <viriyaputra26@gmail.com>
2023-08-03 21:13:36 +0700

    feat: sign in

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Auth } from "./components/Auth";
 
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
+
 function App() {
+  const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
+  const [room, setRoom] = useState(null);
+
+  if (!isAuth) {
+    return (
+      <div>
+        <Auth />
+      </div>
+    );
+  }
+
+  if (room) {
+    return <div> Chat </div>;
+  }
+
   return (
-    <div>
-      <Auth />
+    <div className="room">
+      <label> Enter Room Number</label>
+      <input />
+      <button> Enter Chat</button>
     </div>
   );
 }
--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,10 +1,20 @@
-import {auth, provider} from "../firebase-config.js"
-import {signInWithPopup} from 'firebase/auth'
+import {auth, provider} from "../firebase-config.js";
+import {signInWithPopup} from 'firebase/auth';
+
+import  Cookies  from 'universal-cookie';
+const cookies = new Cookies();
 
 export const Auth = () => {
 
     const signInWithGoogle = async () => {
-        await signInWithPopup(auth, provider);
+        try{
+            const result = await signInWithPopup(auth, provider);
+            cookies.set("auth-token", result.user.refreshToken);
+        }
+        catch(err){
+            console.log(err);
+        }
+
     }
 
     return <div className="auth">
