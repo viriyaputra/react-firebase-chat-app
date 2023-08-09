58f01ba72edaa18c1eceff52f578887abf86c95d
viriyaputra <viriyaputra26@gmail.com>
2023-08-09 19:58:15 +0700

    feat:Chat room & Sign Out

--- src/App.js
+++ src/App.js
@@ -1,32 +1,60 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./App.css";
 import { Auth } from "./components/Auth";
-
+import { Chat } from "./components/Chat";
 import Cookies from "universal-cookie";
+import { signOut } from "firebase/auth"
+import { auth } from "./firebase-config"
+
 const cookies = new Cookies();
 
 function App() {
   const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
   const [room, setRoom] = useState(null);
 
+  const roomInputRef = useRef(null);
+
+  const signUserOut = async () => {
+    await signOut(auth);
+    cookies.remove("auth-token");
+    setIsAuth(false);
+    setRoom(null);
+  };
+
+
   if (!isAuth) {
     return (
       <div>
-        <Auth />
+        <Auth setIsAuth={setIsAuth}/>
       </div>
     );
   }
 
   if (room) {
-    return <div> Chat </div>;
+    return (
+      <>
+      <div>
+        <Chat room={room} />
+      </div>
+      <div className="sign-out">
+        <button onClick={signUserOut}> Sign Out</button>
+      </div>
+      </>
+    );
   }
 
   return (
+    <>
     <div className="room">
-      <label> Enter Room Number</label>
-      <input />
-      <button> Enter Chat</button>
+      <label> Enter Room Name: </label>
+      <input ref={roomInputRef} />
+      <button onClick={() => setRoom(roomInputRef.current.value) }> Enter Chat</button>
     </div>
+
+    <div className="sign-out">
+      <button onClick={signUserOut}> Sign Out</button>
+    </div>
+    </>
   );
 }
 
--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,15 +1,18 @@
 import {auth, provider} from "../firebase-config.js";
 import {signInWithPopup} from 'firebase/auth';
+import "../styles/Auth.css";
 
 import  Cookies  from 'universal-cookie';
 const cookies = new Cookies();
 
-export const Auth = () => {
+export const Auth = (props) => {
+    const {setIsAuth} = props;
 
     const signInWithGoogle = async () => {
         try{
             const result = await signInWithPopup(auth, provider);
             cookies.set("auth-token", result.user.refreshToken);
+            setIsAuth(true);
         }
         catch(err){
             console.log(err);
